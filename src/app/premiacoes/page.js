61c8a622481e3f1a654303c9ee6950bf6099896b
Sara Godinho <saragodinho27@gmail.com>
2026-08-28 16:15:35 -0400

    hshsh

---
+++ src/app/premiacoes/page.js
@@ -0,0 +1,13 @@
+'use client'
+import Link from 'next/link';
+import styles from './page.module.css';
+import Image from 'next/image';
+import { useState } from 'react';
+
+export default function Premiacoes() {
+    return (
+        <main className={styles.premiacoes}>
+            <h1>Teste carai</h1>
+        </main>
+    )
+}
