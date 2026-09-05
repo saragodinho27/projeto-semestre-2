37ce9bc0e61043d01660e6802e53ae358d1db3ba
Sara Godinho <saragodinho27@gmail.com>
2026-09-04 20:47:13 -0400

    asdfghui

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,21 +1,48 @@
 'use client'
 import Link from 'next/link';
+import Image from "next/image";
 import styles from './index.module.css';
-import Image from 'next/image';
+import { ChevronRight, Film, Trophy, Briefcase, Home } from 'lucide-react';
 import { useState } from 'react';
 
-
 export default function Header() {
+    const navItems = [
+        { label: 'Home', href: '/', icon: Home },
+        { label: 'Gêneros', href: '/generos', icon: Film },
+        { label: 'Premiações', href: '/premiacoes', icon: Trophy },
+        { label: 'Profissões', href: '/profissoes', icon: Briefcase },
+    ];
+
     return (
-            <header className={styles.header}>
-                <nav className={styles.cabecario}>
-                    <ul className={styles.ulCabecario}>
-                        <li className={styles.liElements}><Link href='/'>Home</Link></li>
-                        <li className={styles.liElements}><Link href='/generos'>Gêneros</Link></li>
-                        <li className={styles.liElements}><Link href='/premiacoes'>Premiações</Link></li>
-                        <li className={styles.liElements}><Link href='/profissoes'>Profissões</Link></li>
-                    </ul>
-                </nav>
-            </header>
-    )
+        <header className={styles.header}>
+            <div className={styles.logoContainer}>
+                <Link href="/">
+                    <Image 
+                        src="/images/logoCinema.png" 
+                        alt="Logo Cinema"
+                        width={200}
+                        height={85}
+                        priority
+                    />
+                </Link>
+            </div>
+
+            <nav className={styles.cabecario}>
+                <ul className={styles.ulCabecario}>
+                    {navItems.map((item) => {
+                        const Icon = item.icon;
+                        return (
+                            <li key={item.href}>
+                                <Link href={item.href} className={styles.navButton}>
+                                    <Icon className={styles.icon} size={25} />
+                                    <span>{item.label}</span>
+                                    <ChevronRight className={styles.chevron} size={12} />
+                                </Link>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </nav>
+        </header>
+    );
 }
