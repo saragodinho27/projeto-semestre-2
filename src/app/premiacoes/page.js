d5584e03746ebd6aeef0cb36e5e76e335435b99c
Sara Godinho <saragodinho27@gmail.com>
2026-09-08 20:22:47 -0400

    Premiações

--- src/app/premiacoes/page.js
+++ src/app/premiacoes/page.js
@@ -19,50 +19,50 @@ export default function Premiacoes() {
         {
             id: 3,
             nome: "BAFTA",
-            explicacao: "Considerado o 'Oscar britânico', celebra as melhores contribuições britânicas e internacionais para o cinema.",
-            imagemTrofeu: "/images/trofeus/bafta.png"
+            explicacao: "O BAFTA Awards é uma premiação anual organizada pela British Academy of Film and Television Arts, que reconhece as melhores contribuições ao cinema e à televisão. Fundada em 1947, a academia busca promover e desenvolver as artes cinematográficas e televisivas no Reino Unido. A premiação é dividida em diversas categorias, incluindo Melhor Filme, Melhor Diretor, Melhor Ator e Atriz, entre outras. A votação é realizada pelos membros da academia, que somam milhares de profissionais da indústria audiovisual (atores, diretores, produtores, técnicos, etc.)",
+            imagemTrofeu: "/images/Bafta.png"
         },
         {
             id: 4,
             nome: "Cannes",
-            explicacao: "Um dos festivais de cinema mais importantes do mundo, realizado na França, com foco no cinema autoral e independente.",
-            imagemTrofeu: "/images/trofeus/cannes.png"
+            explicacao: "Em 1935, Jean Zay, ministro da Instrução pública e de Belas Artes da França, ao ver a interferência do governo fascista na Mostra de Cinema de Veneza, discursou em favor da criação de uma nova premiação de nível internacional, que não estivesse relacionada com a ideologia do governo italiano da época. Ela premia o melhor filme, melhor ator, melhor atrís, melhor diretor e melhor roteirista. Os vencedores da competição oficial são escolhidos por um júri internacional, sendo eles diretores, atores, produtores e artistas renomados do cinema.",
+            imagemTrofeu: "/images/Cannes.png"
         },
         {
             id: 5,
             nome: "Festival de Veneza",
-            explicacao: "O festival de cinema mais antigo do mundo, realizado na Itália, conhecido por lançar grandes produções artísticas.",
-            imagemTrofeu: "/images/trofeus/veneza.png"
+            explicacao: "O Festival de Veneza é o mais antigo festival de cinema do mundo, e um dos mais influentes da indústria, ao lado de Cannes e Berlin. Realizado no final de agosto ou início de setembro na ilha de Lido, na Lagoa de Veneza, as exibições acontecem no histórico Palazzo del Cinema no Lungomare Marconi. As decisões no Festival de Veneza não são feitas por voto aberto e geral. Os vencedores são decididos por júris internacionais fechados e restritos, compostos por um pequeno grupo de personalidades renomadas do cinema global (diretores, atores, roteiristas, críticos e escritores).",
+            imagemTrofeu: "/images/veneza.png"
         },
         {
             id: 6,
             nome: "Festival de Berlim",
-            explicacao: "Festival de cinema internacional realizado na Alemanha, famoso por seu foco em causas políticas e sociais.",
-            imagemTrofeu: "/images/trofeus/berlim.png"
+            explicacao: "O Festival Internacional de Cinema de Berlim foi criado para o público berlinense em 1951, no início da Guerra Fria, como uma vitrine “do mundo livre”. Moldada pelo turbulento período pós-guerra e pela situação única de uma cidade dividida, a Berlinale se desenvolveu em um local de intercâmbio intercultural e uma plataforma para a exploração cinematográfica crítica das questões sociais. Até hoje é considerado o mais político de todos os grandes festivais de cinema. A decisão do prêmio cabe inteiramente ao Júri Internacional, um comitê restrito e altamente especializado composto por cerca de sete a nove profissionais de destaque no cinema mundial, e o grupo é sempre presidido por uma grande personalidade artística (como Wim Wenders na edição de 2026 ou Todd Haynes em 2025) e inclui diretores, roteiristas, produtores, atores e críticos de cinema internacionais.",
+            imagemTrofeu: "/images/berlim.png"
         },
         {
             id: 7,
             nome: "Emmy",
-            explicacao: "O maior prêmio voltado exclusivamente para produções e profissionais da televisão e do streaming.",
-            imagemTrofeu: "/images/trofeus/emmy.png"
+            explicacao: "Prêmio Emmy , qualquer uma das premiações anuais concedidas por realizações excepcionais na televisão nos Estados Unidos . O nome Emmy deriva de Immy, um apelido para image orthicon, um tubo de câmera usado na televisão. A estatueta do Prêmio Emmy consiste em uma mulher alada, representando a arte, segurando um elétron , representando a ciência. Os Prémios Emmy são criados pela Academia Nacional de Artes e Ciências da Televisão. Somente membros da academia podem votar nos prêmios, e os membros votam apenas dentro de sua própria área de atuação",
+            imagemTrofeu: "/images/emmy.png"
         },
         {
             id: 8,
             nome: "Critics Choice",
-            explicacao: "Premiação concedida pela associação de críticos de cinema dos Estados Unidos e Canadá, servindo de termômetro para o Oscar.",
-            imagemTrofeu: "/images/trofeus/critics.png"
+            explicacao: "Criado em 1995 pela Critics Choice Association (CCA), a maior entidade de críticos dos Estados Unidos e do Canadá, o Critics' Choice Awards se consolidou como uma das premiações mais influentes da temporada de prêmios, servindo como referência para o Oscar ao destacar possíveis favoritos. Especialmente porque é um dos poucos eventos da safra que é transmitido para o mundo inteiro. Por isso, ser indicado ou vencer um Critics Choice Award, aumenta significativamente a visibilidade e a credibilidade de um filme ou série. Quem vota são Críticos e jornalistas.",
+            imagemTrofeu: "/images/CriticsChoice.png"
         },
         {
             id: 9,
             nome: "SAG Awards",
-            explicacao: "Premiação realizada pelo sindicato dos atores de Hollywood (SAG-AFTRA), elegendo as melhores atuações do ano.",
-            imagemTrofeu: "/images/trofeus/sag.png"
+            explicacao: "Considerada uma das premiações mais relevantes da temporada, a cerimônia desempenha um papel fundamental ao aumentar a visibilidade de filmes e artistas que almejam não apenas concorrer, mas principalmente conquistar a estatueta do Oscar. O SAG Awards se destaca por ser a única premiação significativa concedida por atores e para atores, o que amplia sua relevância. Ele também compartilha um número expressivo de votantes com o Oscar. Os membros que votam nas categorias de atuação do Oscar, um grupo composto por mais de 1.000 profissionais da área, incluem membros do Sindicato dos Atores, que também votam no SAG Awards. Embora uma estatueta do SAG Awards frequentemente indique um possível sucesso no Oscar.",
+            imagemTrofeu: "/images/SAGAwards.png"
         },
         {
             id: 10,
             nome: "Grande Otelo",
-            explicacao: "A principal premiação do cinema nacional brasileiro, organizada pela Academia Brasileira de Cinema.",
-            imagemTrofeu: "/images/trofeus/grande_otelo.png"
+            explicacao: "O Prêmio Grande Otelo do Cinema Brasileiro é organizado e votado pelos próprios profissionais do setor, uma forma da própria classe celebrar o seu trabalho e dar o devido reconhecimento ao talento de seus profissionais. A premiação é anual. Contribui para a elevação e a promoção do cinema brasileiro junto à população e ao público do país, através do reconhecimento da qualidade técnica e artística de seus filmes e da confraternização entre os profissionais da indústria.",
+            imagemTrofeu: "/images/GrandeOtelo.png"
         }
     ];
 
